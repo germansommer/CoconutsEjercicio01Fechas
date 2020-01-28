6ea183772ae9380f2bd52b77dd812d7e72c0d18a
germansommer <germangsommer@gmail.com>
2020-01-28 11:33:28 -0300

    Add files via upload

---
+++ CocoFechas01/Dia.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CocoFechas01
+{
+    class Dia
+    {
+        private DateTime fecha;
+        
+        public void setFecha()            
+        {
+            //this.fecha.AddYears(ano);
+            // this.fecha.AddMonths(mes);
+            //this.fecha.AddDays(dia);
+            this.fecha = Dia.cargaFecha();
+        }
+        public DateTime getFecha()
+        {
+            return this.fecha;
+        }
+        static public DateTime cargaFecha()
+        {
+            int dia = 0, mes = 0, ano = 0;
+            //var salida = new Dia();
+
+            do
+            {
+                Console.WriteLine("Ingrese el año: ");
+                try
+                {
+                    ano = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException) { }
+            } while ((ano < 1) || (ano > 2020));            
+            do
+            {
+                Console.WriteLine("Ingrese el mes: ");
+                try
+                {
+                    mes = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException) { }
+            } while ((mes < 1) || (mes > 12));
+            if ((mes == 1) || (mes == 3) || (mes == 5) || (mes == 7) || (mes == 8) || (mes == 10) || (mes == 12))
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese el dia: ");
+                    try
+                    {
+                        dia = int.Parse(Console.ReadLine());
+                    }
+                    catch (FormatException) { }
+                } while ((dia < 1) || (dia > 31));
+            }
+            else
+            {
+                if ((mes == 4) || (mes == 6) || (mes == 9) || (mes == 11))
+                {
+                    do
+                    {
+                        Console.WriteLine("Ingrese el dia: ");
+                        try
+                        {
+                            dia = int.Parse(Console.ReadLine());
+                        }
+                        catch (FormatException) { }
+                    } while ((dia < 1) || (dia > 30));
+                }
+                else
+                {
+                    if (mes == 2)
+                    {
+                        do
+                        {
+                            Console.WriteLine("Ingrese el dia: ");
+                            try
+                            {
+                                dia = int.Parse(Console.ReadLine());
+                            }
+                            catch (FormatException) { }
+                        } while ((dia < 1) || (dia > 28));
+                    }
+                }
+            }            
+            var salida = new DateTime(ano, mes, dia);            
+            return salida;
+        }
+    }
+}
---
+++ CocoFechas01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CocoFechas01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dia1 = new Dia();
+            var dia2 = new Dia();
+            dia1.setFecha();            
+            Console.WriteLine("***************");
+            Console.WriteLine("El dia 1 es: " + dia1.getFecha().ToString("dd, MMMM, yyyy"));
+            Console.WriteLine("***************");
+            dia2.setFecha();
+            Console.WriteLine("***************");
+            Console.WriteLine("El dia 2 es: " + dia2.getFecha().ToString("dd, MMMM, yyyy"));
+            Console.WriteLine("***************");
+            switch (DateTime.Compare(dia1.getFecha(), dia2.getFecha())){
+                case 1:
+                    Console.WriteLine("La diferencia de dias es de: " + ((dia1.getFecha() - dia2.getFecha()).TotalDays));
+                    Console.WriteLine("La diferencia de semanas es de: " + (Math.Round((dia1.getFecha() - dia2.getFecha()).TotalDays/7)));
+                    break;
+                case -1:
+                    Console.WriteLine("La diferencia de dias es de: " + ((dia2.getFecha() - dia1.getFecha()).TotalDays));
+                    Console.WriteLine("La diferencia de semanas es de: " + (Math.Round((dia2.getFecha() - dia1.getFecha()).TotalDays / 7)));
+                    break;
+                case 0:
+                    Console.WriteLine("Las fechas son iguales");
+                    break;
+            }           
+        }
+    }
+}
